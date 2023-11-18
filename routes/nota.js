1dddd45055aee9d0a31603243b2601edfd8c36b2
J Antonio Herrera <jherrera51@alumnos.uaq.mx>
2023-11-17 23:48:39 -0600

    Removed authentication section in index.js and add route to retrieve user-specific notes in nota.js

--- routes/nota.js
+++ routes/nota.js
@@ -3,7 +3,24 @@ const mysql = require('mysql');
 const nota = express.Router();
 const db = require('../config/database');
 
+// Notas de un usuario en especifico
+nota.get("/:id([0-9]{1,3})", async (req, res, next) => {
+    const id = req.params.id;
 
+    console.log(id);
+
+    let searchQuery = "SELECT * FROM nota WHERE id_usuario = ?";
+    let query = mysql.format(searchQuery, [id]);
+    const notas = await db.query(query);
+
+    if (notas.length > 0) {
+        return res.status(200).json({code: 200, message: notas})
+    } else {
+        // No se encontraron notas
+        return res.status(404).send({code: 404, message: "No se encontraron notas"});
+    }
+
+});
 
 /** Borrar al final esta función */
 nota.get("/", async (req, res, next) => {
