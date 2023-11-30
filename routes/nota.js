0c56e93495269ccffe7e1c6c8623f40fd9d97e42
J Antonio Herrera <jherrera51@alumnos.uaq.mx>
2023-11-29 20:56:24 -0600

    Refactor nota.js route to include date conversion and add delete functionality

--- routes/nota.js
+++ routes/nota.js
@@ -9,7 +9,7 @@ nota.get("/:id([0-9]{1,3})", async (req, res, next) => {
 
     console.log(id);
 
-    let searchQuery = "SELECT * FROM nota WHERE id_usuario = ?";
+    let searchQuery = "SELECT *, CONVERT_TZ(fecha, '-00:00', @@global.time_zone) as fecha FROM nota WHERE id_usuario = ? ORDER BY fecha";
     let query = mysql.format(searchQuery, [id]);
     const notas = await db.query(query);
 
@@ -19,7 +19,22 @@ nota.get("/:id([0-9]{1,3})", async (req, res, next) => {
         // No se encontraron notas
         return res.status(404).send({code: 404, message: "No se encontraron notas"});
     }
+});
 
+// Delete a note by its specific id
+nota.delete("/:id([0-9]{1,3})", async (req, res, next) => {
+    const id = req.params.id;
+
+    let deleteQuery = "DELETE FROM nota WHERE id_nota = ?";
+    let query = mysql.format(deleteQuery, [id]);
+    const result = await db.query(query);
+
+    if (result.affectedRows > 0) {
+        return res.status(200).json({code: 200, message: "Nota eliminada correctamente"})
+    } else {
+        // No note found with the given id
+        return res.status(404).send({code: 404, message: "No se encontró la nota"});
+    }
 });
 
 /** Borrar al final esta función */
