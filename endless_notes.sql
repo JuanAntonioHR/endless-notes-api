a52465c8e9c6f58e47452fa9817e7c88f295e65e
J Antonio Herrera <jherrera51@alumnos.uaq.mx>
2023-12-03 09:51:44 -0600

    Updated endless_notes.sql with table structure and data

--- endless_notes.sql
+++ endless_notes.sql
@@ -1,118 +1,86 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.4.27-MariaDB, for Win64 (AMD64)
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 18-11-2023 a las 18:40:12
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: endless_notes
+-- ------------------------------------------------------
+-- Server version	10.4.28-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `endless_notes`
+-- Table structure for table `nota`
 --
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `nota`
---
-
+DROP TABLE IF EXISTS `nota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `nota` (
-  `id_nota` int(11) NOT NULL,
+  `id_nota` int(11) NOT NULL AUTO_INCREMENT,
   `titulo` varchar(50) NOT NULL,
   `texto` varchar(255) NOT NULL,
-  `fecha` datetime NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `categoria` varchar(60) DEFAULT NULL,
   `audio` varchar(255) DEFAULT NULL,
-  `id_usuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`id_nota`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `nota_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `nota`
+-- Dumping data for table `nota`
 --
 
-INSERT INTO `nota` (`id_nota`, `titulo`, `texto`, `fecha`, `categoria`, `audio`, `id_usuario`) VALUES
-(2, 'PRACTICA #5', 'Realizar el diseño de la aplicación', '2023-11-17 10:00:00', 'Escuela', NULL, 0),
-(3, 'ACTIVIDAD #2', 'Finalizar el trabajo', '2023-11-18 08:00:00', 'Escuela', NULL, 0),
-(4, 'PROYECTO #7', 'Entregar el proyecto', '2023-11-18 16:30:00', 'Escuela', NULL, 0),
-(5, 'Dentista', 'Ir al dentista', '2023-11-19 18:30:00', 'Recordatorios', NULL, 0),
-(6, 'Reunión', 'Ir a la ADAcon', '2023-11-19 22:00:00', 'Recordatorios', NULL, 0),
-(7, 'Exámen Artes Visuales', 'Estudiar este día', '2023-11-20 16:00:00', 'Escuela', NULL, 0),
-(8, 'Partido vs Química', 'El partido es en la cancha', '2023-11-20 18:00:00', 'Recordatorios', NULL, 0),
-(9, 'Ir al super', 'Comprar comida', '2023-11-21 21:00:00', 'Recordatorios', NULL, 0),
-(10, 'Revisión de examen de Gongora', 'Ir al salon de gongora a la de ya', '2023-11-21 23:00:00', 'Escuela', NULL, 0),
-(11, 'El sat', 'Ir a declarar al SAT', '2023-11-21 23:30:00', 'Recordatorios', NULL, 0);
-
--- --------------------------------------------------------
+LOCK TABLES `nota` WRITE;
+/*!40000 ALTER TABLE `nota` DISABLE KEYS */;
+INSERT INTO `nota` VALUES (2,'PRACTICA #5','Realizar el diseño de la aplicación','2023-12-01 16:00:00','Escuela',NULL,0),(3,'ACTIVIDAD #2','Finalizar el trabajo','2023-12-02 14:00:00','Escuela',NULL,0),(4,'PROYECTO #7','Entregar el proyecto','2023-12-02 22:30:00','Escuela',NULL,0),(5,'Dentista','Ir al dentista','2023-12-04 00:30:00','Recordatorios',NULL,0),(6,'Reunión','Ir a la ADAcon','2023-12-04 04:00:00','Recordatorios',NULL,0),(7,'Exámen Artes Visuales','Estudiar este día','2023-12-04 22:00:00','Escuela',NULL,0),(8,'Partido vs Química','El partido es en la cancha','2023-12-05 00:00:00','Recordatorios',NULL,0),(9,'Ir al super','Comprar comida','2023-12-06 03:00:00','Recordatorios',NULL,0),(10,'Revisión de examen de Gongora','Ir al salon de gongora a la de ya','2023-12-06 05:00:00','Escuela',NULL,0),(11,'El sat','Ir a declarar al SAT','2023-12-06 05:30:00','Recordatorios',NULL,0),(30,'xd','Hola','2023-12-01 20:33:00',NULL,NULL,0);
+/*!40000 ALTER TABLE `nota` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Table structure for table `usuario`
 --
 
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `correo` varchar(150) NOT NULL,
   `contrasena` varchar(60) NOT NULL,
   `respuesta` varchar(60) DEFAULT NULL,
-  `pregunta` varchar(60) DEFAULT NULL
+  `pregunta` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `usuario`
+-- Dumping data for table `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `nombre`, `correo`, `contrasena`, `respuesta`, `pregunta`) VALUES
-(0, 'Juan Antonio Herrera de la Rosa', 'juanin10toni@gmail.com', '$2b$13$f7xS0hOYft6SuM5Wk3vkwOqX3xwXEKzB2LTpQ2eRgl93e/1CX74Yy', 'Naranja', 'Color favorito');
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (0,'Juan Antonio Herrera de la Rosa','juanin10toni@gmail.com','$2b$13$f7xS0hOYft6SuM5Wk3vkwOqX3xwXEKzB2LTpQ2eRgl93e/1CX74Yy','Naranja','Color favorito');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `nota`
---
-ALTER TABLE `nota`
-  ADD PRIMARY KEY (`id_nota`),
-  ADD KEY `id_usuario` (`id_usuario`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `nota`
---
-ALTER TABLE `nota`
-  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `nota`
---
-ALTER TABLE `nota`
-  ADD CONSTRAINT `nota_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-03  9:50:01
